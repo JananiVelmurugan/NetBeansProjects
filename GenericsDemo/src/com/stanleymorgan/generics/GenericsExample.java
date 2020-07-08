dff874b4141d7373742276ebc4ec6d1e5bea19b4
Janani <jananise@gmail.com>
2020-07-08 20:30:21 +0530

    add

---
+++ GenericsDemo/src/com/stanleymorgan/generics/GenericsExample.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stanleymorgan.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Revature1
+ */
+public class GenericsExample {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        List<String> list = new ArrayList<>(3);
+        
+        
+    }
+    
+    
+    
+    
+}
